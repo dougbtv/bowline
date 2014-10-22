5d31e719c21556e17f06cf6a2376fad3f555169f
Your loyal autobuilder <douglaskippsmith@gmail.com>
2014-10-21 20:03:59 -0400

    [refactor] uses a default config file that's override-able by nomnom options

--- library/Options.js
+++ library/Options.js
@@ -1,63 +1,140 @@
 module.exports = function() {
 
-	var GITHUB_REPO = "dougbtv/docker-asterisk";
+	var CONFIG_FILE_DEFAULT = __dirname + '/../includes/config.json';
+
+	var fs = require('fs');
+
+	// These are required values? for now.
+	var required = [
+		"gituser",
+		"gitrepo",
+		"gitpassword",
+		"docker_user",
+		"docker_email",
+		"docker_password",
+	];
+
+	// We read our config from a json file.
+	this.loadConfig = function(callback) {
+
+		configfile = CONFIG_FILE_DEFAULT;
+		console.log("!trace config? ",configfile);
+
+		fs.exists(configfile,function(exists){
+		
+			if (exists) {
+
+				fs.readFile(configfile, 'utf8', function (err, data) {
+
+					var configs = {};
+
+					if (!err) {
+						var configs = JSON.parse(data);
+
+						console.log("Configuration loaded successfully",configs.CONFIG_NAME);
+
+						callback(configs);
+
+						// console.log("!trace configs: ",configs);
+
+						// -- Connect to mongo.
+
+						/*
+							mongoose.connect(configs.MONGO_CONNECT_STRING);
+
+							var db = mongoose.connection;
+							
+							db.on('error', function(){
+								log.error("mongo_connect",{server: configs.MONGO_CONNECT_STRING });
+							});
+
+							db.once('open', function callback () {
+
+								// We're connected to mongo, now.
+								log.it("mongo_connect",{server: configs.MONGO_CONNECT_STRING });
+
+								// Now that we're connected to mongo, we can continue along.
+								// The Condor object, the meat of our dealings.
+								var Condor = require("./library/Condor.js"); 
+								var condor = new Condor(mongoose,db,log,constants,configs);
+
+							});
+						*/
+
+					} else {
+						throw '!ERROR: FATAL, READING FILE FAILED: ' + err;
+					}
+
+				});
+
+			} else {
+
+				// Unconfigured.
+				callback({});
+			}
+
+		});
+
+	}
 
 	this.parse = function(callback) {
 
-		var opts = require("nomnom")
+		this.loadConfig(function(configs){
+
+			var opts = require("nomnom")
 			.option('gituser', {
 				abbr: 'u',
 				help: 'Github user',
-				required: true
+				// required: true
 			})
 			.option('gitpassword', {
 				abbr: 'p',
 				help: 'Github password',
-				required: true
+				// required: true
 			})
 			.option('docker_user', {
 				help: 'Dockerhub user.',
-				required: true
+				// required: true
 			})
 			.option('docker_email', {
 				help: 'Dockerhub user.',
-				required: true
+				// required: true
 			})
 			.option('docker_password', {
 				help: 'Dockerhub password',
-				required: true
+				// required: true
 			})
 			.option('docker_image', {
-				default: "dougbtv/asterisk",
+				// default: "dougbtv/asterisk",
 				help: 'The docker image that we update'
 			})
 			.option('gitrepo', {
 				abbr: 'r',
-				default: GITHUB_REPO,
+				// default: GITHUB_REPO,
 				help: 'Github repo url in format: user/project'
 			})
 			.option('irc_channel', {
-				default: "##asterisk-autobuilder",
+				// default: "##asterisk-autobuilder",
 				help: 'The bots chanel on IRC'
 			})
 			.option('irc_nick', {
-				default: "ast-autobuild",
+				// default: "ast-autobuild",
 				help: 'The bots nick on IRC'
 			})
 			.option('irc_realname', {
-				default: "asterisk-autobuilder",
+				// default: "asterisk-autobuilder",
 				help: 'The bots "real name" on IRC'
 			})
 			.option('irc_server', {
-				default: "chat.freenode.net",
+				// default: "chat.freenode.net",
 				help: 'The IRC network to connect to'
 			})
 			.option('irc_authuser', {
-				default: "protocoldoug",
+				// default: "protocoldoug",
 				help: 'The IRC network to connect to'
 			})
 			.option('irc_authhost', {
-				default: "unaffiliated/protocoldoug",
+				// default: "unaffiliated/protocoldoug",
 				help: 'The IRC network to connect to'
 			})
 			.option('irc_debug', {
@@ -69,11 +146,11 @@ module.exports = function() {
 				help: 'Do not connect to IRC'
 			})
 			.option('git_setemail', {
-				default: "auto@builder.com",
+				// default: "auto@builder.com",
 				help: 'The IRC network to connect to'
 			})
 			.option('git_setname', {
-				default: "Your loyal autobuilder",
+				// default: "Your loyal autobuilder",
 				help: 'The IRC network to connect to'
 			})
 			.option('authdisabled', {
@@ -94,8 +171,31 @@ module.exports = function() {
 			})
 			.parse();
 
-		callback(opts);
+			console.log("!trace input PRE-CONFIG: ",configs);
 
+			// Alright now that we have those, let's override just what's specified.
+			Object.keys(opts).forEach(function (key) {
+
+				// Alright if they're set here, they override the config.
+				configs[key] = opts[key];
+
+				// target[key];
+
+			});
+
+			console.log("!trace input RECONFIG: ",configs);
+
+			// Now, cycle through the required, and see if they're OK.
+			Object.keys(required).forEach(function (key) {
+				if (typeof configs[required[key]] === 'undefined') {
+					// That's an error.
+					throw "Hey man, you're missing a required config: " + required[key];
+				}
+			});
+
+			callback(configs);
+
+		});
 
 	}.bind(this);	
 
