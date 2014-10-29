1ad4ec178cc59d7df0b2a1285ef31c271d125ba0
Your loyal autobuilder <douglaskippsmith@gmail.com>
2014-10-29 09:14:36 -0400

    [formatting] convert knotsController to tabs

--- www/app/scripts/controllers/knotsController.js
+++ www/app/scripts/controllers/knotsController.js
@@ -1,75 +1,75 @@
 bowlineApp.controller('knotsController', ['$scope', '$location', '$http', 'loginModule', 'releaseModule', 'ENV', function($scope,$location,$http,login,release,ENV) {
 
-  	$scope.params = $location.search();
-    $scope.is_owner = false;
+		$scope.params = $location.search();
+		$scope.is_owner = false;
 
-    $scope.getSingleRelease = function() {
+		$scope.getSingleRelease = function() {
 
-      $scope.is_owner = false;
+			$scope.is_owner = false;
 
-      release.getSingleRelease($scope.params.details,function(err,single){
+			release.getSingleRelease($scope.params.details,function(err,single){
 
-        if (!err) {
+				if (!err) {
 
-          $scope.single = single;
-          // console.log("!trace checking single release owner: ",single.owner,login.fulluser._id,(single.owner == login.fulluser._id));
-          
-          // Are we the owner of this release?
-          if (single.owner == login.fulluser._id) {
-            $scope.is_owner = true;
-          }
+					$scope.single = single;
+					// console.log("!trace checking single release owner: ",single.owner,login.fulluser._id,(single.owner == login.fulluser._id));
+					
+					// Are we the owner of this release?
+					if (single.owner == login.fulluser._id) {
+						$scope.is_owner = true;
+					}
 
-        } else {
-          $scope.error = err;
-        }
-        
+				} else {
+					$scope.error = err;
+				}
+				
 
-      });
+			});
 
-    }
+		}
 
-    $scope.stopJob = function(id) {
-      console.log("!trace stopJob id: ",id);
-      release.stopJob(id,function(err){
-        $scope.getSingleRelease();
-      });
-    }
+		$scope.stopJob = function(id) {
+			console.log("!trace stopJob id: ",id);
+			release.stopJob(id,function(err){
+				$scope.getSingleRelease();
+			});
+		}
 
-    $scope.ago = function(indate) {
-      if (indate) {
-        return new moment(indate).fromNow();
-      } else {
-        return "Never";
-      }
-      
-    }
+		$scope.ago = function(indate) {
+			if (indate) {
+				return new moment(indate).fromNow();
+			} else {
+				return "Never";
+			}
+			
+		}
 
-    // Ok bring up the details link.
-    $scope.showDetails = function(id) {
-      $location.search('details', id);
-    }
+		// Ok bring up the details link.
+		$scope.showDetails = function(id) {
+			$location.search('details', id);
+		}
 
-    // And instantiate.
+		// And instantiate.
 
-    if ($scope.params.details) {
+		if ($scope.params.details) {
 
-      $scope.getSingleRelease();
+			$scope.getSingleRelease();
 
-    } else {
+		} else {
 
-      release.getReleases(function(err,rels){
+			release.getReleases(function(err,rels){
 
-        if (!err) {
+				if (!err) {
 
-          $scope.releases = rels;
-          console.log("!trace checking releases: ",rels);
-        } else {
-          $scope.error = err;
-        }
-        
+					$scope.releases = rels;
+					console.log("!trace checking releases: ",rels);
+				} else {
+					$scope.error = err;
+				}
+				
 
-      });
+			});
 
-    }
+		}
 
 }]);
