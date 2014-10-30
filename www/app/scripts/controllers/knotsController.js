5ba5f5b6b62a3e99a722b17e889e5ae18d644129
Your loyal autobuilder <douglaskippsmith@gmail.com>
2014-10-30 14:30:42 -0400

    [bugfix][spinner] vastly improves spinner by creating directive

--- www/app/scripts/controllers/knotsController.js
+++ www/app/scripts/controllers/knotsController.js
@@ -2,26 +2,6 @@
 
 bowlineApp.controller('knotsController', ['$scope', '$location', '$http', 'loginModule', 'releaseModule', 'ENV', function($scope,$location,$http,login,release,ENV) {
 
-		var opts = {
-		  lines: 11, // The number of lines to draw
-		  length: 2, // The length of each line
-		  width: 3, // The line thickness
-		  corners: 1, // Corner roundness (0..1)
-		  radius: 6, // The radius of the inner circle
-		  rotate: 0, // The rotation offset
-		  direction: 1, // 1: clockwise, -1: counterclockwise
-		  color: '#000', // #rgb or #rrggbb or array of colors
-		  speed: 0.8, // Rounds per second
-		  trail: 40, // Afterglow percentage
-		  shadow: false, // Whether to render a shadow
-		  hwaccel: false, // Whether to use hardware acceleration
-		  className: 'spinner', // The CSS class to assign to the spinner
-		  zIndex: 2e9, // The z-index (defaults to 2000000000)
-		  top: '50%', // Top position relative to parent
-		  left: '50%' // Left position relative to parent
-		};
-		var target = document.getElementById('spinneroo');
-		var spinner = new Spinner(opts).spin(target);
 
 		$scope.params = $location.search();
 		$scope.is_owner = false;
