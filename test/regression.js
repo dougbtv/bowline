a440c578c4809599e42c9acff06aa7fad4cc6544
Your loyal autobuilder <douglaskippsmith@gmail.com>
2014-11-17 14:41:09 -0500

    [units] has create/update/delete tests

--- test/regression.js
+++ test/regression.js
@@ -11,7 +11,7 @@ var TEST_PASSWORD = 'testertester';
 
 // Start up bowline.
 var bowline;
-bowline = fork('./bowline.js',['--logfile','/tmp/bowline.log']);
+bowline = fork('./bowline.js',['--logfile','/tmp/bowline.log','--skipautostart']);
 
 exports.waitForServer = function(test) {
 	setTimeout(function(){
@@ -76,9 +76,110 @@ exports.loginSuccessful = function(test){
 	
 };
 
+var NEW_RELEASE = {
+	session: {
+		username: TEST_USERNAME,
+		password: TEST_PASSWORD
+	},
+	release: {
+		hook_secret: "fb6a6f72-e71a-415b-b725-48c14c79a732",
+		branch_name: "autobuild",
+		branch_master: "master",
+		slug: "helloworld",
+		docker_tag: "dougbtv/helloworld",
+		store_local: true,
+		git_repo: "dougbtv/test-dockerfiles",
+		git_path: "/helloworld/Dockerfile",
+		method: "hook",
+		git_method: "github"
+	}
+};
+
+var new_releaseid;
+
+exports.addRelease = function(test){
+	
+	client.post('/api/addRelease', NEW_RELEASE, function(err, req, res, data) {
+
+		var result = JSON.parse(res.body);
+
+		// console.log("!trace NEW RELEASE: ",result);
+		
+		if (!err) {
+			if (!result.error) {
+				new_releaseid = result.releaseid;
+				test.ok(true, "Release added");
+			} else {
+				test.ok(false, "New release errored out");	
+			}
+			// console.log(res.body);
+		} else {
+			test.ok(false, "Restify error: " + err);
+		}
+		test.done();
+	});
+	
+};
+
+exports.editRelease = function(test){
+
+	var edited_release = NEW_RELEASE;
+	edited_release.release._id = new_releaseid;
+	edited_release.release.branch_name = "edited";
+	
+	// console.log("!trace edited_release: ",edited_release);
+
+	client.post('/api/editRelease', edited_release, function(err, req, res, data) {
+
+		var result = JSON.parse(res.body);
+
+		// console.log("!trace editRelease result: ",result);
+		
+		if (!err) {
+			if (!result.error) {
+				test.ok((new_releaseid == result.releaseid), "Release edited");
+			} else {
+				test.ok(false, "Edit release errored out");	
+			}
+			// console.log(res.body);
+		} else {
+			test.ok(false, "Restify error: " + err);
+		}
+		test.done();
+	});
+	
+};
+
+exports.deleteRelease = function(test){
+
+	client.post('/api/deleteRelease', {
+		releaseid: new_releaseid,
+		session: {
+			username: TEST_USERNAME,
+			password: TEST_PASSWORD
+		}
+	}, function(err, req, res, data) {
+
+		var result = JSON.parse(res.body);
+
+		if (!err) {
+			if (!result.error) {
+				test.ok(true, "Release deleted");
+			} else {
+				test.ok(false, "Delete release errored out");	
+			}
+			// console.log(res.body);
+		} else {
+			test.ok(false, "Restify error: " + err);
+		}
+		test.done();
+	});
+	
+};
+
 /*
 exports.testSomethingElse = function(test){
-	test.ok(false, "this assertion should fail");
+	test.ok(false,"this assertion should fail");
 	test.done();
 };
 */
