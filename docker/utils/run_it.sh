2cb1c52713add3d2dc270145dca7f9e247b29b95
dougbtv <douglaskippsmith@gmail.com>
2014-11-27 08:12:12 -0500

    [interim] experimenting with ditching DinD which is a pain, and mounting unix socket into bowline container

--- docker/utils/run_it.sh
+++ docker/utils/run_it.sh
@@ -6,13 +6,13 @@ echo "Removing ALL containers..."
 docker kill $(docker ps -a -q) || true
 docker rm $(docker ps -a -q) || true
 
-echo "Starting docker-in-docker"
-docker run \
-  --privileged \
-  --name dind \
-  -d -p 4444:4444 \
-  -e PORT=4444 \
-  -t jpetazzo/dind:latest
+# echo "Starting docker-in-docker"
+# docker run \
+#  --privileged \
+#  --name dind \
+#  -d -p 4444:4444 \
+#  -e PORT=4444 \
+#  -t jpetazzo/dind:latest
 
 echo "Starting registry server..."
 # docker run -p 5000:5000 --name regserver -d -t registry
@@ -22,8 +22,9 @@ echo "Starting mongodb..."
 docker run -d -p 27017:27017 -v $MONGO_DIR:/data/db --name mongodb dockerfile/mongodb
 
 echo "Starting bowline..."
-docker run -p 8000:8000 --link dind:dind --name bowline -i -t dougbtv/bowline
+docker run -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v /bowline/ --name bowline -d -t dougbtv/bowline
+# --link dind:dind 
 
 echo "Starting nginx..."
-docker run -p 80:80 -p 443:443 --link regserver:regserver --name -i -t dougbtv/bowline-nginx
+docker run -p 80:80 -p 443:443 --volumes-from bowline --link regserver:regserver -d -t dougbtv/bowline-nginx
 
