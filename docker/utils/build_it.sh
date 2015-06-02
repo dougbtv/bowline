1527c6767a95560ccd4cc5c110bcb42ef9b9f722
Will Barnwell <wwbarnwell@gmail.com>
2015-06-02 13:55:49 -0400

    Revert "sudo all docker commands"
    This reverts commit 4fc31bf6c4f31562d48c794b055368051cf70068.

--- docker/utils/build_it.sh
+++ docker/utils/build_it.sh
@@ -5,37 +5,37 @@ EXAMPLE_DATA=$(pwd)/../../db/
 # ------------------ Basic requirements.
 
 echo "Pulling required docker containers...."
-sudo docker pull library/mongo:latest
-sudo docker pull jpetazzo/dind:latest
-sudo docker pull nginx:latest
-sudo docker pull svendowideit/ambassador:latest
+docker pull library/mongo:latest
+docker pull jpetazzo/dind:latest
+docker pull nginx:latest
+docker pull svendowideit/ambassador:latest
 
 echo "Copying sample config 
 to add to Bowline container"
 cp ../../includes/example.config.json ../bowline/config.json
 
 echo "Building bowline specific docker containers..."
-sudo docker build -t dougbtv/bowline ../bowline/.
-sudo docker build -t dougbtv/bowline-nginx ../nginx/.
+docker build -t dougbtv/bowline ../bowline/.
+docker build -t dougbtv/bowline-nginx ../nginx/.
 
 # ------------------- Preload Mongo data.
 
 echo "Removing ALL containers..."
-sudo docker kill $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 
 echo "Running mongo for preload..."
 rm -Rf $MONGO_DIR
 mkdir $MONGO_DIR
-sudo docker run -d -p 27017:27017 -v $MONGO_DIR:/data/db --name temp_mongo library/mongo
+docker run -d -p 27017:27017 -v $MONGO_DIR:/data/db --name temp_mongo library/mongo
 
 CMD_RELEASE_IMPORT=""
 CMD_USER_IMPORT=""
 
 echo "Starting mongo client"
-sudo docker run -it --rm --link temp_mongo:mongo -v $EXAMPLE_DATA:/exampledata/ library/mongo \
+docker run -it --rm --link temp_mongo:mongo -v $EXAMPLE_DATA:/exampledata/ library/mongo \
     bash -c 'mongoimport --host mongo --db bowline --collection releases --file /exampledata/json/releases.json; mongoimport --host mongo --db bowline --collection users --file /exampledata/json/users.json'
 
 echo "Cleaning up all containers...."
-sudo docker kill $(docker ps -a -q) || true
-sudo docker rm $(docker ps -a -q) || true
+docker kill $(docker ps -a -q) || true
+docker rm $(docker ps -a -q) || true
--- docker/utils/run_it.sh
+++ docker/utils/run_it.sh
@@ -3,8 +3,8 @@ MONGO_DIR=/tmp/mongodata
 REGISTRY_DIR=/tmp/registry
 
 echo "Removing ALL containers..."
-sudo docker kill $(docker ps -a -q) || true
-sudo docker rm $(docker ps -a -q) || true
+docker kill $(docker ps -a -q) || true
+docker rm $(docker ps -a -q) || true
 
 # echo "Starting docker-in-docker"
 # docker run \
@@ -16,17 +16,17 @@ sudo docker rm $(docker ps -a -q) || true
 
 echo "Starting registry server..."
 # docker run -p 5000:5000 --name regserver -d -t registry
-sudo docker run -d -e GUNICORN_OPTS=[--preload] -v $REGISTRY_DIR:/registry -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/registry -e SEARCH_BACKEND=sqlalchemy -p 5000:5000  --name regserver registry:latest
+docker run -d -e GUNICORN_OPTS=[--preload] -v $REGISTRY_DIR:/registry -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/registry -e SEARCH_BACKEND=sqlalchemy -p 5000:5000  --name regserver registry:latest
 
 echo "Starting mongo..."
-sudo docker run -d -p 27017:27017 -v $MONGO_DIR:/data/db --name mongo library/mongo
+docker run -d -p 27017:27017 -v $MONGO_DIR:/data/db --name mongo library/mongo
 
 echo "Starting nginx ambassador..."
 HOST_IP_ADDRESS=$(ifconfig | grep -a2 docker0 | grep -P "inet[^6]" | awk '{print $2}')
-sudo docker run -d --name nginx-amb --expose 443 -e NGINX_PORT_443_TCP=tcp://$HOST_IP_ADDRESS:443 svendowideit/ambassador
+docker run -d --name nginx-amb --expose 443 -e NGINX_PORT_443_TCP=tcp://$HOST_IP_ADDRESS:443 svendowideit/ambassador
 
 echo "Starting bowline..."
-sudo docker run \
+docker run \
     -p 8000:8000 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /bowline/ \
@@ -35,7 +35,7 @@ sudo docker run \
     --name bowline -d -t dougbtv/bowline
 
 echo "Starting nginx..."
-sudo docker run \
+docker run \
     -p 80:80 -p 443:443 \
     --volumes-from bowline \
     --link bowline:bowline \
