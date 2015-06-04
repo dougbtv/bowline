005b0f1d9ccdb4de1afed607b6774c981518376e
Will Barnwell <wwbarnwell@gmail.com>
2015-06-04 09:54:34 -0400

    Testing :rw
    rather than use --privileged we can use the more elegant and secure :rw
    and avoid giving the container full root access on the host.

--- docker/utils/build_it.sh
+++ docker/utils/build_it.sh
@@ -33,7 +33,7 @@ CMD_RELEASE_IMPORT=""
 CMD_USER_IMPORT=""
 
 echo "Starting mongo client"
-docker run -it --rm --link temp_mongo:mongo -v $EXAMPLE_DATA:/exampledata/ library/mongo \
+docker run -it --rm --link temp_mongo:mongo -v $EXAMPLE_DATA:/exampledata/:rw library/mongo \
     bash -c 'mongoimport --host mongo --db bowline --collection releases --file /exampledata/json/releases.json; mongoimport --host mongo --db bowline --collection users --file /exampledata/json/users.json'
 
 echo "Cleaning up all containers...."
