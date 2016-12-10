ab61dd18c3d6965cf192d5df91925de8c4018419
dougbtv <douglaskippsmith@gmail.com>
2016-12-10 17:05:03 -0500

    [data][bugfix] export had bogus output winding up in json

--- db/export.sh
+++ db/export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker exec -it mongo mongoexport --db bowline --collection releases > json/releases.json
-docker exec -it mongo mongoexport --db bowline --collection users > json/users.json
+docker exec -it mongo mongoexport --quiet --db bowline --collection releases > json/releases.json
+docker exec -it mongo mongoexport --quiet --db bowline --collection users > json/users.json
 
